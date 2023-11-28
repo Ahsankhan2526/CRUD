38ac4d5ce4a2f7107d8d9b927a12e673f18839ee
Muhammad Ahsan Khan <muhammadahsankhan2526@gmail.com>
2023-11-28 21:52:36 +0500

    starting LS

--- CRUD.js
+++ CRUD.js
@@ -1,7 +1,5 @@
 let addUserBtn = document.querySelector(".add-user-btn");
-
-let testDiv = document.childNodes[1].childNodes[2].childNodes[3]
-// console.log('testDiv: ', testDiv);
+let testDiv = document.querySelector(".test");
 
 let input = testDiv.childNodes[3];
 let ul = testDiv.childNodes[7];
@@ -11,65 +9,64 @@ let editBtn;
 
 let editText;
 
+function addUser() {
+    // let student =  input.value;
+    // let LSdata = localStorage.getItem("Student Name");
+    // if (!LSdata) {
+    //     LSdata = []
+    // }
+    // else{
+    //     LSdata =(JSON.parse(LSdata));
+    // }
+    // LSdata.push(student);
+    // input.value = '';
+    // localStorage.setItem('Student Name', JSON.stringify(LSdata) )
+    // console.log(LSdata);
 
 
-function addUser(){
-    if (input.value.trim().length > 2 && addUserBtn.innerHTML === 'Add User') {
-        // creating li
-        liEl = document.createElement('li');
-        let liText = document.createTextNode(input.value);
-        
-        // creating delete btn
-        delBtn = document.createElement('button');
-        let delBtnTxt = document.createTextNode('Delete');
-    delBtn.setAttribute('onclick', 'delet(this)')
+  if (input.value.trim().length > 2 && addUserBtn.innerHTML === "Add User") {
+    // creating li
+    liEl = document.createElement("li");
+    let liText = document.createTextNode(input.value);
+
+    // creating delete btn
+    delBtn = document.createElement("button");
+    let delBtnTxt = document.createTextNode("Delete");
+    delBtn.setAttribute("onclick", "delet(this)");
     delBtn.appendChild(delBtnTxt);
     liEl.appendChild(liText);
     liEl.appendChild(delBtn);
-    
+
     // creating edit btn
-    editBtn = document.createElement('button');
-    let editBtnTxt = document.createTextNode('Edit');
-    editBtn.setAttribute('onclick', 'edit(this)')
+    editBtn = document.createElement("button");
+    let editBtnTxt = document.createTextNode("Edit");
+    editBtn.setAttribute("onclick", "edit(this)");
     editBtn.appendChild(editBtnTxt);
     liEl.appendChild(editBtn);
-    
+
     ul.appendChild(liEl);
-    input.value = ''
-}
-if (addUserBtn.innerHTML === 'update') {
+    input.value = "";
+  }
+  if (addUserBtn.innerHTML === "update") {
     ToEdit.nodeValue = input.value;
-    addUserBtn.innerHTML ='Add User';
-    input.value = '';
-    ToEdit.nextSibling.removeAttribute('disabled');
-    ToEdit.nextSibling.nextSibling.removeAttribute('disabled');
-
+    addUserBtn.innerHTML = "Add User";
+    input.value = "";
+    ToEdit.nextSibling.removeAttribute("disabled");
+    ToEdit.nextSibling.nextSibling.removeAttribute("disabled");
+  }
 }
+function delet(del) {
+  ul.removeChild(del.parentNode);
 }
-function delet (del) {
-    ul.removeChild(del.parentNode);
-}
-
 
 let ToEdit;
 function edit(edt) {
-    ToEdit = edt.previousSibling.previousSibling;    
-    input.value = ToEdit.nodeValue;
-    
-    
-    
-    
-    
-    
-    
-    // console.log('edt: ', edt);
-    // let ulParent = edt.parentNode.parentNode;
-    // let editChild = edt.parentNode;
-    // editText = editChild.childNodes[0].nodeValue;
-    
-    addUserBtn.innerHTML = 'update';
-    edt.setAttribute('disabled', 'disabled')
-    edt.previousSibling.setAttribute('disabled', 'disabled')
+  ToEdit = edt.previousSibling.previousSibling;
+  input.value = ToEdit.nodeValue;
+
+  addUserBtn.innerHTML = "update";
+  edt.setAttribute("disabled", "disabled");
+  edt.previousSibling.setAttribute("disabled", "disabled");
 }
 
 
@@ -78,3 +75,33 @@ function edit(edt) {
 
 
 
+
+
+
+
+// let inputEl = document.querySelectorAll('.inputEL')[0];
+
+function addStd() {
+    // let student =  inputEl.value;
+    // let LSdata = localStorage.getItem("Student Name");
+    // if (!LSdata) {
+    //     LSdata = []
+    // }
+    // else{
+    //     LSdata =(JSON.parse(LSdata));
+    // }
+    // LSdata.push(student);
+    // inputEl.value = '';
+    // localStorage.setItem('Student Name', JSON.stringify(LSdata) )
+    // console.log(LSdata);
+    
+}
+function Onload(){
+    // let ul = document.querySelector('.ul');
+    // let LSdata = JSON.parse(localStorage.getItem('Student Name'))
+    // console.log(LSdata);
+    // console.log(ul.childNodes);
+    // for (let i = 0; i < LSdata.length; i++) {
+    //     ul.innerHTML += `<li>${LSdata[i]}</li>`
+    // }
+}
